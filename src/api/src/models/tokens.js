a07e50718123a6982a63006613cba3762d9acb64
elinab26 <neveteli2005@gmail.com>
2025-06-05 17:50:29 +0300

    EX1-346: changed the implementation of the token to be jwt

--- src/api/src/models/tokens.js
+++ src/api/src/models/tokens.js
@@ -1,13 +1,18 @@
 const Users = require('./users')
+const jwt = require("jsonwebtoken")
+const key = "Hello how are you this is a key that will help for generating the token"
 
 function login(email, password) {
     const users = Users.getAllUsers()
     const user = users.find(u => u.email === email)
     if (user && user.password === password) {
-        return user.id;
+        const data = { username: user }
+        const token = jwt.sign(data, key);
+        return { token }
     } else {
         return -1;
     }
 }
 
 module.exports = { login }
+
