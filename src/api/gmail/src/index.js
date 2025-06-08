7cce1cddd3e6de6dee16e12b914b24a8aabb6a57
elinab26 <neveteli2005@gmail.com>
2025-06-08 10:06:21 +0300

    Added the basic React app

---
+++ src/api/gmail/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import LeftMenu from './LeftMenu';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <LeftMenu />
+  </React.StrictMode>
+);
+
