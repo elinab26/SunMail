a0c3f81c82edab87eb60fbfcf0f70e446f5ca5d8
elinab26 <neveteli2005@gmail.com>
2025-06-15 19:39:56 +0300

    EX1-405: added the get and delete functions

--- src/api/src/models/labelsAndUser.js
+++
@@ -1,7 +0,0 @@
-const User = require('./users')
-
-exports.addLabelToUser = (user, label, userId) => {
-    const labels = User.getLabelsOfUser(user);
-    labels.push(label);
-    return labels.at(labels.length - 1)
-}
---
+++ src/api/src/models/labelsAndUsers.js
@@ -0,0 +1,23 @@
+const User = require('./users')
+
+exports.addLabelToUser = (user, label) => {
+    const labels = User.getLabelsOfUser(user);
+    labels.push(label);
+    return labels.at(labels.length - 1)
+}
+
+exports.getLabelFromUserById = (user, label) => {
+    const labels = User.getLabelsOfUser(user);
+    return labels.find(l => l == label);
+}
+
+exports.deleteLabelFromUser = (user, label) => {
+    const labels = User.getLabelsOfUser(user);
+    let i = labels.indexOf(label)
+    if (i > -1) {
+        labels.splice(i, 1)
+    } else {
+        return -1
+    }
+    return 0;
+}
