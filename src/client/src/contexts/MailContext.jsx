2d139df0df5ccb610cb7ce6806d2bec2f371d925
elinab26 <neveteli2005@gmail.com>
2025-06-25 16:55:47 +0300

    fixed bug

--- src/client/src/contexts/MailContext.jsx
+++ src/client/src/contexts/MailContext.jsx
@@ -23,10 +23,7 @@ export function MailProvider({ children }) {
             setAllMails(await res.json());
         }
     }
-    useEffect(() => {
-        fetchAllMails();
-    }, []);
-
+    
     async function fetchMails(currentFolder) {
         var res;
         res = await fetch(`/api/mails/label/${currentFolder}`, {
